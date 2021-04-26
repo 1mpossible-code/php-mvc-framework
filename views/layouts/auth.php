33e267fcf020c8f220457de5474cc9e589acdf9b
Max <linme00p@gmail.com>
2021-04-26 15:56:49 +0300

    Create auth layout

---
+++ views/layouts/auth.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Hello, world!</title>
+</head>
+<body>
+
+<div class="container">
+    {{content}}
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
