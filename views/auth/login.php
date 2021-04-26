81e31b70afb542ab2c7daa791026f835f6ea1973
Max <linme00p@gmail.com>
2021-04-26 15:28:19 +0300

    Create login and register views Add register and login links in main template

---
+++ views/auth/login.php
@@ -0,0 +1,13 @@
+<h1>Login</h1>
+<form action="" method="POST">
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+        <div class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ views/auth/register.php
@@ -0,0 +1,21 @@
+<h1>Register</h1>
+<form action="" method="POST">
+    <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input type="text" name="name" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+        <div class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password confirm</label>
+        <input type="password" name="password-confirmation" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
