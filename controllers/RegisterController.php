c602564508ae0b031febcf51d33d267574e61fde
Max <linme00p@gmail.com>
2021-04-29 14:16:56 +0300

    Create RegisterModel class Create RegisterModel class to enable registration page. Update RegisterController due to the model

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -6,6 +6,8 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\core\Response;
+use app\models\RegisterModel;
 
 /**
  * Class RegisterController
@@ -19,8 +21,11 @@ class RegisterController extends Controller
      */
     public function index()
     {
+        $registerModel = new RegisterModel();
         $this->setLayout('auth');
-        return $this->render('auth/register');
+        return $this->render('auth/register', [
+            'model' => $registerModel,
+        ]);
     }
 
     /**
@@ -30,6 +35,18 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        return 'handle submitted data';
+        // Create an instance of RegisterModel
+        $registerModel = new RegisterModel();
+        // Fulfill the model with the data from request
+        $registerModel->loadData($request->getBody());
+        // Return success if the validation is passed and new user is created
+        if ($registerModel->validate() && $registerModel->register()) {
+            return "Success";
+        }
+        // TODO: Implement response redirecting
+        $this->setLayout('auth');
+        return $this->render('auth/register', [
+            'model' => $registerModel,
+        ]);
     }
 }
