05848da01a3cd77d1edb8eca79c3cffaa0662b0a
Max <linme00p@gmail.com>
2021-04-30 15:19:50 +0300

    Update User with unique email Restrict registration with the same email addresses

--- models/User.php
+++ models/User.php
@@ -97,7 +97,7 @@ class User extends DbModel
         // Rules for attributes
         return [
             'name' => [self::RULE_REQUIRED],
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [self::RULE_UNIQUE, 'class' => self::class]],
             'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => '8'], [self::RULE_MAX, 'max' => '255']],
             'passwordConfirm' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
         ];
