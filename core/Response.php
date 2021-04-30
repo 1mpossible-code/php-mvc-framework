e10d5498a591779a96bbf2603df8c9bd67aa6be4
Max <linme00p@gmail.com>
2021-04-30 15:23:32 +0300

    Update Response Implement redirection in Response

--- core/Response.php
+++ core/Response.php
@@ -4,10 +4,27 @@
 namespace app\core;
 
 
+/**
+ * Class Response
+ * @package app\core
+ */
 class Response
 {
-    public function setStatusCode(int $code)
+    /**
+     * Set specified status code
+     * @param int $code
+     */
+    public function setStatusCode(int $code): void
     {
         http_response_code($code);
+    }
+
+    /**
+     * Redirect to specified url
+     * @param string $url
+     */
+    public function redirect(string $url): void
+    {
+        header('Location: ' . $url);
     }
 }
