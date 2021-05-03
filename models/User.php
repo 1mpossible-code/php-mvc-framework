f510af057ca0661a2b885ef15cb635363cb1085c
Max <linme00p@gmail.com>
2021-05-03 13:40:15 +0300

    Add labels to User Add data labels for User model

--- models/User.php
+++ models/User.php
@@ -102,4 +102,18 @@ class User extends DbModel
             'passwordConfirm' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
         ];
     }
+
+    /**
+     * @inheritDoc
+     * @return string[]
+     */
+    public function labels(): array
+    {
+        return [
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'passwordConfirm' => 'Repeat password',
+        ];
+    }
 }
